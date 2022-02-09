36f4c5657d49178608c333cc88d289fc8e7759fb
Eder Canada <canada.eder@gmail.com>
2022-02-09 10:19:01 -0800

    responsive background

--- menu.js
+++ menu.js
@@ -1,20 +1,45 @@
-
-// const formRow = document.querySelector('.home-row');
-
-const contactForm = document.querySelector('#contact-form-id')
+const contactForm = document.querySelector('#contact-form-id');
 const createWithUsButton = document.querySelector('.create-button');
 const hamburgerMenuPhone = document.querySelector('#hamburger-menu-phone');
 
-function showHideContactForm() {
-  if (!hamburgerMenuPhone.classList.contains('collapsed')) {
-    createWithUsButton.classList.remove('hidden');
-    contactForm.classList.add('active-form')
-    
-
+window.addEventListener('resize', function () {
+  let win = this;
+  if (win.innerWidth < 900) {
+    console.log('smaller');
   } else {
-    createWithUsButton.classList.add('hidden');
-    contactForm.classList.remove('active-form')
+    console.log('bigger');
+  }
+});
+
+function showHideContactForm() {
+  if (window.screen.width > 576) {
+    console.log('does not work');
+  } else {
+    if (!hamburgerMenuPhone.classList.contains('collapsed')) {
+      createWithUsButton.classList.remove('hidden');
+      contactForm.classList.add('active-form');
+    } else {
+      createWithUsButton.classList.add('hidden');
+      contactForm.classList.remove('active-form');
+    }
   }
 }
 
 hamburgerMenuPhone.addEventListener('click', showHideContactForm);
+
+
+window.addEventListener('resize', function () {
+  let win = this;
+  if (win.innerWidth > 576) {
+    contactForm.classList.remove('active-form');
+    createWithUsButton.classList.add('hidden');
+  } else {
+    if (!hamburgerMenuPhone.classList.contains('collapsed')) {
+      createWithUsButton.classList.remove('hidden');
+      contactForm.classList.add('active-form');
+    } else {
+      createWithUsButton.classList.add('hidden');
+      contactForm.classList.remove('active-form');
+    }
+  }
+});
