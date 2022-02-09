c415b28b5861cb6699135c8987a2078093d344de
Eder Canada <canada.eder@gmail.com>
2022-02-09 10:22:32 -0800

    comment out code

--- menu.js
+++ menu.js
@@ -2,15 +2,6 @@ const contactForm = document.querySelector('#contact-form-id');
 const createWithUsButton = document.querySelector('.create-button');
 const hamburgerMenuPhone = document.querySelector('#hamburger-menu-phone');
 
-window.addEventListener('resize', function () {
-  let win = this;
-  if (win.innerWidth < 900) {
-    console.log('smaller');
-  } else {
-    console.log('bigger');
-  }
-});
-
 function showHideContactForm() {
   if (window.screen.width > 576) {
     console.log('does not work');
@@ -27,19 +18,19 @@ function showHideContactForm() {
 
 hamburgerMenuPhone.addEventListener('click', showHideContactForm);
 
-
 window.addEventListener('resize', function () {
   let win = this;
   if (win.innerWidth > 576) {
     contactForm.classList.remove('active-form');
     createWithUsButton.classList.add('hidden');
   } else {
-    if (!hamburgerMenuPhone.classList.contains('collapsed')) {
-      createWithUsButton.classList.remove('hidden');
-      contactForm.classList.add('active-form');
-    } else {
-      createWithUsButton.classList.add('hidden');
-      contactForm.classList.remove('active-form');
-    }
+    // if (!hamburgerMenuPhone.classList.contains('collapsed')) {
+    //   createWithUsButton.classList.remove('hidden');
+    //   contactForm.classList.add('active-form');
+    // } else {
+    //   createWithUsButton.classList.add('hidden');
+    //   contactForm.classList.remove('active-form');
+    // }
+    console.log('click');
   }
 });
