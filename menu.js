e75a669a9abd22e1cc434a1cb93622b654cef9c8
Eder Canada <canada.eder@gmail.com>
2022-02-08 16:12:53 -0800

    drop form

---
+++ menu.js
@@ -0,0 +1,10 @@
+const navbarTogglerIcon = document.querySelector('.navbar-toggler-icon');
+const formRow = document.querySelector('.home-row');
+
+navbarTogglerIcon.addEventListener('click', () => {
+  formRow.style.marginTop = '3rem';
+});
+
+navbarTogglerIcon.addEventListener('click', () => {
+  formRow.style.marginTop = '3rem';
+});
