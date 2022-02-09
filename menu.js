ed7e2fe77ed1e8e5a9653914018ce5d5d26bda1c
Eder Canada <canada.eder@gmail.com>
2022-02-08 16:17:06 -0800

    add height

--- menu.js
+++ menu.js
@@ -1,10 +1,9 @@
 const navbarTogglerIcon = document.querySelector('.navbar-toggler-icon');
 const formRow = document.querySelector('.home-row');
+const homeContainer = document.querySelector('.home-container')
 
 navbarTogglerIcon.addEventListener('click', () => {
   formRow.style.marginTop = '3rem';
-});
+  homeContainer.style.height = '120vh'
 
-navbarTogglerIcon.addEventListener('click', () => {
-  formRow.style.marginTop = '3rem';
 });
